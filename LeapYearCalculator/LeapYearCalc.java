4429f810765842ee0c557eb3bc7461898be9c872
grantpg180 <62108947+grantpg180@users.noreply.github.com>
2020-06-01 22:25:35 -0400

    tests for leap year status between 1 and 999

--- LeapYearCalculator/LeapYearCalc.java
+++ LeapYearCalculator/LeapYearCalc.java
@@ -2,11 +2,18 @@ package LeapYearCalculator;
 
 public class LeapYearCalc {
 
-    public boolean calculateYear(int year) {
+    public boolean calculateYearsUnder999(int year) {
         if (year > 0 && year < 1000){
             return true;
         }else {
             return false;
         }
 }
+    public void calculateLeapYearsBelow1000(int year) {
+        if (year % 4 == 0){
+            System.out.println("is a leap year");  
+        }else if (year % 4 != 0){
+            System.out.println("is NOT a leap year");
+        }
+    }
 }
--- LeapYearCalculator/LeapYearCalcTest.java
+++ LeapYearCalculator/LeapYearCalcTest.java
@@ -8,10 +8,20 @@ import org.junit.Test;
 public class LeapYearCalcTest {
     
     @Test
-    public final void yearsBelow1000(){
+    public final void testsYearsBelow1000(){
     LeapYearCalc year = new LeapYearCalc();
-        assertTrue("500", year.calculateYear(500));
-        assertTrue("999", year.calculateYear(999));
+        assertTrue("500", year.calculateYearsUnder999(500));
+        assertTrue("999", year.calculateYearsUnder999(999));
 
     }
+    @Test
+    public void calculateLeapYearsBelow1000(){
+        LeapYearCalc year = new LeapYearCalc();
+        assertEquals("400", year.calculateLeapYearsBelow1000(400));
+        assertEquals("322", year.calculateLeapYearsBelow1000(322));
+
+
+    }
+
+   
 }
